26cc5ee863311d19094e0405d46edc05abbb98ce
Walison J. Deus <walison.josededeus@gmail.com>
2020-09-28 17:13:25 -0300

    ajustes Permissão Localização

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -14,6 +14,7 @@ import {
   TouchableHighlight,
   ScrollView,
   TextInput,
+  PermissionsAndroid
 } from "react-native";
 import MapView, { Marker, Callout, AnimatedRegion } from "react-native-maps";
 
